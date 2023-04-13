1a43639416b6c4f1e70717afdfbd80eb9a59e4fe
YuiZoldyck <betasan28@gmail.com>
2023-04-13 10:21:56 -0600

    added other files

---
+++ IntexProject2/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using IntexProject2.Models.ViewModels;
+
+namespace IntexProject2.Infrastructure
+{
+    // Helps build the next page button
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PaginationTagHelper : TagHelper
+    {
+        // Dynamically create page links
+
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper(IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        public PageInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            output.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
---
+++ IntexProject2/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace IntexProject2.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BurialsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        // Calculate num pages needed
+        public int TotalPages => (int)Math.Ceiling((double)TotalNumBooks / BurialsPerPage);
+    }
+}
+
+
