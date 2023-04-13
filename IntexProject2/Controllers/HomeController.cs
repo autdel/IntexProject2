1297e39bf421792649d01857673b8e1e7c4b28a2
Kate Opager <opagerk@byu.edu>
2023-04-13 10:30:31 -0600

    ..

--- IntexProject2/Controllers/HomeController.cs
+++ IntexProject2/Controllers/HomeController.cs
@@ -34,6 +34,10 @@ namespace IntexProject2.Controllers
         {
             return View();
         }
+        public IActionResult CreateEntry()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
