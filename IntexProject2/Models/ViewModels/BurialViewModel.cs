f34496b7a99c97d534d3203b9abb2cbc5afe0f3f
YuiZoldyck <betasan28@gmail.com>
2023-04-13 10:21:56 -0600

    added other files

---
+++ IntexProject2/Models/ViewModels/BurialViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+namespace IntexProject2.Models.ViewModels
+{
+    public class BurialViewModel
+    {
+        public IQueryable<Burialmain> Burials { get; set; }
+        public PageInfo PageInfo { get; set; }
+
+    }
+}
