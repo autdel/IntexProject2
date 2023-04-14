df1082896243aee8b96a92fc09af26d12f35825d
autdel32 <autdel@byu.edu>
2023-04-13 18:35:07 -0600

    BurialInfo page

--- IntexProject2/Repository/IBurialsRepository.cs
+++ IntexProject2/Repository/IBurialsRepository.cs
@@ -16,18 +16,27 @@ namespace IntexProject2.Repository
         public List<Textile> GetAllTextiles();
         public Analysis GetAnalysisByTextileID(long textileID);
         public Bodyanalysis GetBodyAnalysisByBodyAnalysisID(int baKeyID);
-        public List<Color> GetColorsByTextileID(long textileID);
+        public Color GetColorByTextileID(long textileID);
         public Decoration GetDecorationByTextileID(long textileID);
-        public List<Dimension> GetDimensionsByTextileID(long textileID);
-        public List<Photodata> GetPhotoDataByTextileID(long textileID);
-        public List<Structure> GetStructuresByTextileID(long textileID);
-        public List<Textilefunction> GetTextileFunctionByTextileID(long textileID);
-        public List<Yarnmanipulation> GetYarnManipulationByTextileID(long textileID);
-        
+        public Dimension GetDimensionByTextileID(long textileID);
+        public Photodata GetPhotoDataByTextileID(long textileID);
+        public Structure GetStructureByTextileID(long textileID);
+        public Textilefunction GetTextileFunctionByTextileID(long textileID);
+        public Yarnmanipulation GetYarnManipulationByTextileID(long textileID);
+
 
 
 
         #endregion
+
+        // ----------------------------------------- BURIAL INFO GET METHODS -------------------------------------------//
+        #region
+        public Bodyanalysiskey GetBodyanalysiskeyByBurialID(long burialID);
+        public Bodyanalysis GetBodyanalysisByBurialID(long burialID);
+        public Burialmain GetBurialmainByBurialID(long burialID);
+        public Textile GetTextileByBurialID(long burialID);
+        #endregion
+
         // ----------------------------------------- CREATE METHODS -------------------------------------------//
         #region
 
